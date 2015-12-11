db5b25fe36a69d5d2645230aab811905c54cb645
xd98714 <holger.grosse-plankermann@codecentric.de>
2015-12-11 14:02:26 +0100

    ETS

--- lib/registry.ex
+++ lib/registry.ex
@@ -6,26 +6,30 @@ defmodule KV.Registry do
   @doc """
   Starts the registry.
   """
-  def start_link(event_manager, buckets, opts \\ []) do
-    # 1. Pass the buckets supervisor as argument
-    GenServer.start_link(__MODULE__, {event_manager, buckets}, opts)
+  def start_link(table, event_manager, buckets, opts \\ []) do
+    # 1. We now expect the table as argument and pass it to the server
+    GenServer.start_link(__MODULE__, {table, event_manager, buckets}, opts)
   end
 
-
   @doc """
-  Looks up the bucket pid for `name` stored in `server`.
+  Looks up the bucket pid for `name` stored in `table`.
 
-  Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
+  Returns `{:ok, pid}` if a bucket exists, `:error` otherwise.
   """
-  def lookup(server, name) do
-    GenServer.call(server, {:lookup, name})
+  def lookup(table, name) do
+    # 2. lookup now expects a table and looks directly into ETS.
+    #    No request is sent to the server.
+    case :ets.lookup(table, name) do
+      [{^name, bucket}] -> {:ok, bucket}
+      [] -> :error
+    end
   end
 
   @doc """
   Ensures there is a bucket associated to the given `name` in `server`.
   """
   def create(server, name) do
-    GenServer.cast(server, {:create, name})
+    GenServer.call(server, {:create, name})
   end
 
   @doc """
@@ -36,44 +40,39 @@ defmodule KV.Registry do
   end
 
   ## Server Callbacks
+  def init({table, events, buckets}) do
+    refs = :ets.foldl(fn {name, pid}, acc ->
+      HashDict.put(acc, Process.monitor(pid), name)
+    end, HashDict.new, table)
 
-  def init({events, buckets}) do
-    names = HashDict.new
-    refs  = HashDict.new
-    # 2. Store the buckets supervisor in the state
-    {:ok, %{names: names, refs: refs, events: events, buckets: buckets}}
+    {:ok, %{names: table, refs: refs, events: events, buckets: buckets}}
   end
 
-  def handle_call({:lookup, name}, _from, state) do
-    {:reply, HashDict.fetch(state.names, name), state}
-  end
+  # 4. The previous handle_call callback for lookup was removed
 
+  def handle_call({:create, name}, _from, state) do
+    case lookup(state.names, name) do
+      {:ok, pid} ->
+        {:reply, pid, state} # Reply with pid
+        :error ->
+          {:ok, pid} = KV.Bucket.Supervisor.start_bucket(state.buckets)
+          ref = Process.monitor(pid)
+          refs = HashDict.put(state.refs, ref, name)
+          :ets.insert(state.names, {name, pid})
+          GenEvent.sync_notify(state.events, {:create, name, pid})
+          {:reply, pid, %{state | refs: refs}} # Reply with pid
+        end
+      end
 
+      def handle_info({:DOWN, ref, :process, pid, _reason}, state) do
+        # 6. Delete from the ETS table instead of the HashDict
+        {name, refs} = HashDict.pop(state.refs, ref)
+        :ets.delete(state.names, name)
+        GenEvent.sync_notify(state.events, {:exit, name, pid})
+        {:noreply, %{state | refs: refs}}
+      end
 
-  def handle_cast({:create, name}, state) do
-    if HashDict.get(state.names, name) do
-      {:noreply, state}
-    else
-      # 3. Use the buckets supervisor instead of starting buckets directly
-      {:ok, pid} = KV.Bucket.Supervisor.start_bucket(state.buckets)
-      ref = Process.monitor(pid)
-      refs = HashDict.put(state.refs, ref, name)
-      names = HashDict.put(state.names, name, pid)
-      GenEvent.sync_notify(state.events, {:create, name, pid})
-      {:noreply, %{state | names: names, refs: refs}}
+      def handle_info(_msg, state) do
+        {:noreply, state}
+      end
     end
-  end
-
-  def handle_info({:DOWN, ref, :process, pid, _reason}, state) do
-    {name, refs} = HashDict.pop(state.refs, ref)
-    names = HashDict.delete(state.names, name)
-    # 4. Push a notification to the event manager on exit
-    GenEvent.sync_notify(state.events, {:exit, name, pid})
-    {:noreply, %{state | names: names, refs: refs}}
-  end
-
-  def handle_info(_msg, state) do
-    {:noreply, state}
-  end
-
-end
