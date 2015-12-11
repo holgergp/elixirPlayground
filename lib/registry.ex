88a72461c21ca5f3b04410c0d9ddb81501d8b706
xd98714 <holger.grosse-plankermann@codecentric.de>
2015-12-11 10:28:24 +0100

    With GenEvent

--- lib/registry.ex
+++ lib/registry.ex
@@ -6,9 +6,11 @@ defmodule KV.Registry do
   @doc """
   Starts the registry.
   """
-  def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, :ok, opts)
-  end
+  def start_link(event_manager, opts \\ []) do
+      # 1. start_link now expects the event manager as argument
+      GenServer.start_link(__MODULE__, event_manager, opts)
+    end
+
 
   @doc """
   Looks up the bucket pid for `name` stored in `server`.
@@ -29,46 +31,50 @@ defmodule KV.Registry do
   @doc """
    Stops the registry.
    """
-   def stop(server) do
-     GenServer.call(server, :stop)
-   end
+  # def stop(server) do
+  #   GenServer.call(server, :stop)
+  # end
 
   ## Server Callbacks
 
-  def init(:ok) do
-    names = HashDict.new
-    refs  = HashDict.new
-    {:ok, {names, refs}}
-  end
-
-  def handle_call({:lookup, name}, _from, {names, _} = state) do
-    {:reply, HashDict.fetch(names, name), state}
-  end
-
-  def handle_call(:stop, _from, state) do
-    {:stop, :normal, :ok, state}
-  end
-
-  def handle_cast({:create, name},  {names, refs}) do
-    if HashDict.has_key?(names, name) do
-      {:noreply, {names, refs}}
-    else
-      {:ok, pid} = KV.Bucket.start_link()
-      ref = Process.monitor(pid)
-      refs = HashDict.put(refs, ref, name)
-      names = HashDict.put(names, name, pid)
-      {:noreply, {names, refs}}
+  def init(events) do
+      # 2. The init callback now receives the event manager.
+      #    We have also changed the manager state from a tuple
+      #    to a map, allowing us to add new fields in the future
+      #    without needing to rewrite all callbacks.
+      names = HashDict.new
+      refs  = HashDict.new
+      {:ok, %{names: names, refs: refs, events: events}}
     end
-  end
 
-  def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
-    {name, refs} = HashDict.pop(refs, ref)
-    names = HashDict.delete(names, name)
-    {:noreply, {names, refs}}
-  end
+  def handle_call({:lookup, name}, _from, state) do
+       {:reply, HashDict.fetch(state.names, name), state}
+   end
+
+  def handle_cast({:create, name}, state) do
+      if HashDict.get(state.names, name) do
+        {:noreply, state}
+      else
+        {:ok, pid} = KV.Bucket.start_link()
+        ref = Process.monitor(pid)
+        refs = HashDict.put(state.refs, ref, name)
+        names = HashDict.put(state.names, name, pid)
+        # 3. Push a notification to the event manager on create
+        GenEvent.sync_notify(state.events, {:create, name, pid})
+        {:noreply, %{state | names: names, refs: refs}}
+      end
+    end
+
+    def handle_info({:DOWN, ref, :process, pid, _reason}, state) do
+        {name, refs} = HashDict.pop(state.refs, ref)
+        names = HashDict.delete(state.names, name)
+        # 4. Push a notification to the event manager on exit
+        GenEvent.sync_notify(state.events, {:exit, name, pid})
+        {:noreply, %{state | names: names, refs: refs}}
+      end
 
   def handle_info(_msg, state) do
     {:noreply, state}
   end
-  
+
 end
--- test/registry_test.exs
+++ test/registry_test.exs
@@ -1,9 +1,35 @@
+defmodule Forwarder do
+    use GenEvent
+
+    def handle_event(event, parent) do
+      send parent, event
+      {:ok, parent}
+    end
+  end
+
+
+
+
+
+
 defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
   setup do
-    {:ok, registry} = KV.Registry.start_link
+    {:ok, manager} = GenEvent.start_link
+    {:ok, registry} = KV.Registry.start_link(manager)
+
+    GenEvent.add_mon_handler(manager, Forwarder, self())
     {:ok, registry: registry}
+  end
+  
+  test "sends events on create and crash", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    assert_receive {:create, "shopping", ^bucket}
+
+    Agent.stop(bucket)
+    assert_receive {:exit, "shopping", ^bucket}
   end
 
   test "spawns buckets", %{registry: registry} do
